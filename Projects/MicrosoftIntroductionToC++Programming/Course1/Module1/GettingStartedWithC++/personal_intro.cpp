b9403c909844bc61ad4e7172bc1b48f86f92d1e9
MartijnHoek <martynhoek@gmail.com>
2026-06-08 10:49:16 +0200

    Completed excersises for Course 1 Module 1 Segment Getting Started With C++

---
+++ Projects/MicrosoftIntroductionToC++Programming/Course1/Module1/GettingStartedWithC++/personal_intro.cpp
@@ -0,0 +1,66 @@
+/*
+ * Personal Introduction
+ * Created by: Martijn Hoek
+ * Date: 08/06/2026
+ * 
+ * Description: This program displays personal information about myself in a formatted way in the terminal.
+*/
+
+/* 
+ * My Learning Goals:
+ * 1. The basics of C++ syntax and structure, including how to write a simple program and use the main function.
+ * 2. More advanced concepts.
+ */
+
+// Include header
+#include <iostream>
+
+// Main Function
+int main() {
+
+    // Display Header
+    std::cout << "==============================" << std::endl;
+    std::cout << "    Personal Introduction     " << std::endl;
+    std::cout << "==============================" << std::endl;
+
+    // Display basic information
+    std::cout << "Hello, my name is Martijn Hoek." << std::endl;
+    std::cout << "I am a Automotive Engineer working as a Software Developer." << std::endl;
+    std::cout << "I am originally from Barendrecht, but now living in Tiel." << std::endl;
+    std::cout << "My favorite programming language is Python, but I am excited to learn C++." << std::endl;
+    
+    // Education
+    std::cout << "\nEducation:" << std::endl;
+    std::cout << "------------------------------" << std::endl;
+    std::cout << "Degree: Bachelor of Science in Automotive Engineering" << std::endl;
+    std::cout << "School: Rotterdam University of Applied Sciences" << std::endl;
+    std::cout << "Graduation Year: 2022" << std::endl;
+
+    // Career Goals
+    std::cout << "\nCareer Goals:" << std::endl;
+    std::cout << "------------------------------" << std::endl;
+    std::cout << "Short-term: To become proficient in C++ and contribute to software development projects." << std::endl;
+    std::cout << "Long-term: To work on innovative software solutions in the automotive industry." << std::endl;
+
+    // Why I am taking this course
+    std::cout << "\nWhy I am taking this course:" << std::endl;
+    std::cout << "------------------------------" << std::endl;
+    std::cout << "I am taking this course to expand my programming skills and to learn C++" << std::endl;
+
+    // Favorite programming concepts
+    std::cout << "\nTopics I'm Exited About:" << std::endl;
+    std::cout << "------------------------------" << std::endl;
+    std::cout << "C++" << std::endl;
+    std::cout << "Further deep dive into Object-Oriented Programming" << std::endl;
+    std::cout << "Memory Management and Pointers" << std::endl;
+
+    // Closing message
+    std::cout << "\nThank you for taking the time to read my introduction!" << std::endl;
+
+    // Footer
+    std::cout << "==============================" << std::endl;
+
+
+
+    return 0;
+}
